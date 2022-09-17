9e1491ed0d3ccca849773eef30fdf7f69ea5a00b
Nidumolu1974 <113469115+Nidumolu1974@users.noreply.github.com>
2022-09-16 20:27:00 -0400

    Initial Commit

---
+++ GitHubRepository/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubRepository
+//
+//  Created by Krishan Dev  on 16/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubRepository/GitHubRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoryApp.swift
+//  GitHubRepository
+//
+//  Created by Krishan Dev  on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
